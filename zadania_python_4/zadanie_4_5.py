7293fd29495149a27e1871d4e1412655dd2021f6
Alex <36136484+homosum1@users.noreply.github.com>
2021-11-14 11:10:03 +0100

    Update zadanie_4_5.py

--- zadania_python_4/zadanie_4_5.py
+++ zadania_python_4/zadanie_4_5.py
@@ -1,2 +1,52 @@
+# zakładam poprawność danych wejściowych, to znaczy left i right nie wychodzą poza zakres listy
 
-def iterative_reverse(L, left, right)
+def iterative_reverse(L, left, right):
+    if left > right:
+        buf = left
+        left = right
+        right = buf
+    
+    iteration_range = (right - left) // 2
+    if left == 0:
+        iteration_range += 1
+    
+    for i in range(iteration_range):
+        buf2 = L[left]
+        L[left] = L[right]
+        L[right] = buf2
+
+        left += 1
+        right -= 1
+
+def recursive_reverse(L, left, right):
+    buf = L[left]
+    L[left] = L[right]
+    L[right] = buf
+
+    if left < right:
+        return recursive_reverse(L, left + 1, right - 1)
+    else:
+        return
+
+
+print("\niterative example: (0 - 5)")
+
+L = [0,1,2,3,4,5]
+print(L)
+iterative_reverse(L, 0, 5)
+print(L)
+
+print("\nexample 2: (0 - 4)")
+
+K = [0,1,2,3,4,5]
+print(K)
+iterative_reverse(K, 0, 4)
+print(K)
+
+
+print("\nrecursive example: (1 - 5)\n")
+
+W = [0,1,2,3,4,5]
+print(W)
+recursive_reverse(W, 1, 5)
+print(W)
